924ccdc34a2ec2821200faf0f8fd4a2b484559ef
Tom Stratton <tstratton3912@gmail.com>
2020-10-15 17:59:57 +0100

    adding title, reading UTC time using Date method, adding button to later change the time to e.g. Shanghai time

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
+
+
+
+
+var date = new Date();
+// console.log(date);
+let hr = date.getUTCHours();
+let min = date.getUTCMinutes();
+let sec = date.getUTCSeconds();
+
+let hrPosition = hr*360/12 + min*(360/12)/60;
+let minPosition = min*360/60 + sec*(360/60)/60;
+let secPosition = sec*360/60;
+
+HOURHAND.style.transform = "rotate(" + hrPosition + "deg)"; 
+MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)"; 
+SECONDHAND.style.transform = "rotate(" + secPosition + "deg)"; 
