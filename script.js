93a1aedb1d404a79fda953ddf454aba6b25f28ae
Tom Stratton <tstratton3912@gmail.com>
2020-10-18 17:19:13 +0100

    making buttons to display times in different cities

--- script.js
+++ script.js
@@ -7,9 +7,14 @@ const SECONDHAND = document.querySelector("#second");
 
 var date = new Date();
 // console.log(date);
-let hr = date.getUTCHours();
-let min = date.getUTCMinutes();
-let sec = date.getUTCSeconds();
+
+//Default time
+var hr = date.getUTCHours();
+var min = date.getUTCMinutes();
+var sec = date.getUTCSeconds();
+
+var time = [hr, min, sec];
+
 
 let hrPosition = hr*360/12 + min*(360/12)/60;
 let minPosition = min*360/60 + sec*(360/60)/60;
@@ -18,3 +23,24 @@ let secPosition = sec*360/60;
 HOURHAND.style.transform = "rotate(" + hrPosition + "deg)"; 
 MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)"; 
 SECONDHAND.style.transform = "rotate(" + secPosition + "deg)"; 
+
+//Other time zones 
+
+function londontime() {
+    let hrPosition = (hr+1)*360/12 + min*(360/12)/60;
+    HOURHAND.style.transform = "rotate(" + hrPosition + "deg)"; 
+}
+
+function paristime() {
+    let hrPosition = (hr+2)*360/12 + min*(360/12)/60;
+    HOURHAND.style.transform = "rotate(" + hrPosition + "deg)"; 
+}
+
+function utctime() {
+    let hrPosition = (hr)*360/12 + min*(360/12)/60;
+    HOURHAND.style.transform = "rotate(" + hrPosition + "deg)"; 
+}
+
+
+
+
